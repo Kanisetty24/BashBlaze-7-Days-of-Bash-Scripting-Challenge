cf94120d3529ac379aec4a0e59ea183fd6e6d516
Kanisetty24 <165292638+Kanisetty24@users.noreply.github.com>
2024-05-24 18:31:25 +0530

    Update task_1_completed.sh

--- Challenges/Day_4/task_1_completed.sh
+++ Challenges/Day_4/task_1_completed.sh
@@ -7,29 +7,30 @@ if [ -z "$1" ]; then
 fi
 
 # Check if the process is running
-input=$(sudo systemctl status "$1" | grep -i running)
+status=$(sudo systemctl is-active "$1")
 
 # If the process is not running, attempt to restart it
-if [ -z "$input" ]; then
+if [ "$status" != "active" ]; then
     count=0
-    echo "We are attempting to restart the process $1 ..."
-    while [ $count -l 3 ]; do
-        #sudo systemctl restart "$1"
+    while [ $count -lt 3 ]; do
+        echo "Attempting to restart the process $1 ..."
+        sudo systemctl restart "$1"
         sleep 5 # Wait for a few seconds before checking again
-        input=$(sudo systemctl status "$1" | grep -i running)
+        status=$(sudo systemctl is-active "$1")
 
         # If the process is running after restart, exit loop
-        if [ -n "$input" ]; then
-            echo "The $1 process has been successfully restarted"
+        if [ "$status" == "active" ]; then
+            echo "The $1 process has been successfully restarted."
             exit 0
         fi
         ((count++)) # Increment the retry count
     done
 
     # If unable to restart after 3 attempts, display error message and exit
-    echo "Failed to restart the $1 process after 3 attempts."
+    echo "Maximum restart attempts reached. Please check the process manually."
     exit 1
 else
     # If the process is already running, display message
-    echo "The $1 process is already running"
+    echo "The $1 process is already running."
 fi
+
